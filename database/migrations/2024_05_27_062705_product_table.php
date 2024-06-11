6211eb961195f75e91c27ea3bf9a5b81732c51bb
Nathaniel Orion <70615987+orion-gabriel@users.noreply.github.com>
2024-06-11 14:42:41 +0700

    Add files via upload

--- database/migrations/2024_05_27_062705_product_table.php
+++ database/migrations/2024_05_27_062705_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class ProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('image'); #gambar file
+            $table->string('name'); #gambar file
+            $table->string('description');
+            $table->integer('stock');
         });
     }
 
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
