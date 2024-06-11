42fd9ef4ef4ed8dde2253cb355933cbd913b8c38
Nathaniel Orion <70615987+orion-gabriel@users.noreply.github.com>
2024-06-11 14:37:15 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('login');
+});
+
+//idk dibawah buat url2 nyas
+Route::get('/addItem', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
     return view('register');
 });
+
+
+//yang bawah ini buat login register, masih on work
+Route::post('login', [AuthController::class, 
+    'login'])->name('login');
+
+    Route::post('register', [AuthController::class, 
+    'register'])->name('register');
+
+    Route::post('logout', [AuthController::class, 
+    'logout'])->name('logout');
+
+Route::get('register', [AuthController::class, 
+    'index_register'])->name('index_register')
+    ;
+
+Route::get('login', [AuthController::class, 
+   'index_login'])->name('index_login')
+   ;
+
+Route::get('home', [HomeController::class,'index_home'])->name('index_home');
+// Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+
+// Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+// Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
